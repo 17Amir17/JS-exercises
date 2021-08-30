a781254f138991279f8d2e4b211d0a82022cf3c1
17Amir17 <amirangel101@hotmail.com>
2021-08-30 13:28:24 +0300

    Update quadraticEquation.js
    for strict

--- quadraticEquation.js
+++ quadraticEquation.js
@@ -3,7 +3,8 @@
 const a = 1;
 const b = -6;
 const c = 9;  // stands for x**2 - 6*x + 9 = 0
-let solutionA = solutionB = 0
+let solutionA;
+let solutionB = 0
 // your code goes here
 solutionA = (-b + Math.sqrt(b**2 - 4*a*c)) / 2*a;
 solutionB = (-b - Math.sqrt(b**2 - 4*a*c)) / 2*a;
