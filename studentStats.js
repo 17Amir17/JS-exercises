dfe439f1a1dbc477f9ff3baeda4b75f57c406341
17Amir17 <amirangel101@hotmail.com>
2021-08-30 13:55:04 +0300

    added get median
    added get median function

--- studentStats.js
+++ studentStats.js
@@ -8,4 +8,13 @@ function getRange(grades){
     return max-min;
 }
 
+function getMedian(grades){
+    // Sort the array, return the number in the index of the center of the array (index must be an integer so I convert it into a number)
+   let middleIndex = grades.length/2;
+   let sorted = grades.slice(); //Copy the array so that i do not modify its order
+   sorted.sort()
+   return sorted[Number(middleIndex)];
+}
+
 let range = getRange(grades);
+let median = getMedian(grades);
