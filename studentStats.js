9dde405128c60f7c0904a8e86874dd0f7775f9de
17Amir17 <36531255+17Amir17@users.noreply.github.com>
2021-08-31 10:42:16 +0300

    Update studentStats.js

--- studentStats.js
+++ studentStats.js
@@ -9,9 +9,13 @@ function getRange(grades){
     return max-min;
 }
 
+function getMiddleIndex(grades){
+    return Math.floor(grades.length/2);
+}
+
 function getMedian(grades){
     // Sort the array, return the number in the index of the center of the array (index must be an integer so I convert it into a integer by doing math.floor)
-   let middleIndex = Math.floor(grades.length/2);
+   let middleIndex = getMiddleIndex(grades);
    let sorted = grades.slice(); //Copy the array so that i do not modify its order
    sorted.sort();
    return sorted[Number(middleIndex)];
@@ -19,7 +23,7 @@ function getMedian(grades){
 
 function getSecondHalfRange(grades){
     //I want to split the array in half and run the getRange function on it
-    let middleIndex = grades.length/2;
+    let middleIndex = getMiddleIndex(grades);
     let secondHalf = grades.slice(middleIndex, grades.length);
     return getRange(secondHalf);
 }
