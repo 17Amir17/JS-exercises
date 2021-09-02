de3341b09ea8aadfa11381d4b8f93569c2fd0a7a
17Amir17 <36531255+17Amir17@users.noreply.github.com>
2021-09-02 11:42:18 +0300

    Update caesarCipher13.js
    remove console.log

--- caesarCipher13.js
+++ caesarCipher13.js
@@ -3,7 +3,6 @@
 function rot13(str) {
   let newStr = ""
   str.split("").forEach((c) => newStr += (/[a-zA-Z]/).test(c) ? String.fromCharCode((c.charCodeAt() - 65 + 13) % 65 % 26 + 65): c)
-  console.log(newStr)
   return newStr;
 }
 
